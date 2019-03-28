88dda275f062891ad8cb92af2f06765842fe17a6
GoogleSensei <gooooglesensei@gmail.com>
2019-03-29 02:09:34 +0900

    check when icon on click

--- src/js/background.js
+++ src/js/background.js
@@ -1,14 +1,29 @@
 
-window.onload = function() {
+(window.onload = function() {
+
+    var currentTab;
+
+    chrome.tabs.query( {active: true, currentWindow: true}, function(tabs) {
+        var currentTab = tabs[0];
+        console.log(currentTab.url);
+    });
+
 	chrome.tabs.query( {currentWindow: true}, function (tabs) {
-        // var tab = tabs[0];
+        console.log(tabs);
+        Array.prototype.getLastVal = function (){ return this[this.length -1];}
+        var currentTab = tabs.getLastVal();
+        console.log(currentTab.url);
 
         // アクティブなウィンドウのタブのURLを全て取得
-        for (i = 0; i <= tabs.length ; i++) {
-            var tab = tabs[i];
-            console.log(tab.url);
+        for (i = 0; i < tabs.length -1 ; i++) {
+            var openedTab = tabs[i];
+            console.log(openedTab.url);
+
+            // 開いているタブと、今開いたタブのURLをみて、同じだったらポップアップを出す
+            if ( currentTab.url === openedTab.url) {
+                chrome.tabs.remove(currentTab.id);
+            }
         }
         
-        // 開いているタブと、今開いたタブのURLをみて、同じだったらポップアップを出す
 	});
-}
+})();
